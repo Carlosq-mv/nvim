17d5d0cbb5fe0c601fd02ff5b7d5c3279a6e6bb2
carlosq-mv <cquiroz7869@gmail.com>
2026-07-08 16:31:32 -0700

    update settings to fix bug not rendering italic and bold text

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,21 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "*",
+			callback = function()
+				vim.api.nvim_set_hl(0, "@markup.italic", { italic = true })
+				vim.api.nvim_set_hl(0, "@markup.strong", { bold = true })
+			end,
+		})
+		-- vim.api.nvim_set_hl(0, "@markup.italic", { italic = true })
+		-- vim.api.nvim_set_hl(0, "@markup.strong", { bold = true })
+	end,
 }
